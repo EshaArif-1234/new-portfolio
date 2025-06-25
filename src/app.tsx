0af47da00103688abca09f649b8638330b0ef2cb
EshaArif-1234 <eshaarif0322@gmail.com>
2025-06-25 21:10:43 +0500

    comment the banner

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import { BrowserRouter } from "react-router-dom"  ;
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import Footer from "./components/footer";
 
-import { Navbar, Hero, About, Experience, Works, Contact, Footer } from "./components";
-
+// App
 const App = () => {
-  const [hide, setHide] = useState(false);
+  const [hide, setHide] = useState(true);
 
   return (
+    <BrowserRouter>
     <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
       <div className="relative z-0">
         <Navbar hide={hide} />
         <Hero />
       </div>
+        </div>
       <About />
       <Experience />
       <Works />
       <Contact />
       <Footer />
     </div>
-  );
+    </BrowserRouter>  );
 };
 
 export default App;
